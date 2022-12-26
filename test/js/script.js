4bcb68521670cc9c2ab818d36215a9e2ad6c11ba
sasholk <iccc97@gmail.com>
2022-12-26 16:14:15 +0200

    third  practice  with Cycles in test folder

--- test/js/script.js
+++ test/js/script.js
@@ -1,49 +1,16 @@
 "use strict";
 
-function firstTask() {
-  // Значения массива менять нельзя, тут он проверяется автоматически именно на эти значения
-  const arr = [3, 5, 8, 16, 20, 23, 50];
-  const result = [];
+const lines = 5;
+let result = "";
 
-  // Пишем решение вот тут
-  for (let i = 0; i < arr.length; i++) {
-    result[i] = arr[i];
+for (let i = 0; i <= lines; i++) {
+  for (let j = 0; j < lines - i; j++) {
+    result += " ";
   }
-  console.log(result);
-  // Не трогаем
-  return result;
+  for (let j = 0; j < 2 * i + 1; j++) {
+    result += "*";
+  }
+  result += "\n";
 }
 
-// Место для второй задачи
-function secondTask() {
-  // Значения массива менять нельзя, тут он проверяется автоматически именно на эти значения
-  const data = [5, 10, "Shopping", 20, "Homework"];
-
-  // Пишем решение вот тут
-  for (let i = 0; i < data.length; i++) {
-    if (typeof data[i] === "number") {
-      data[i] = data[i] * 2;
-    } else if (typeof data[i] === "string") {
-      data[i] = `${data[i]} - done`;
-    }
-  }
-  console.log(data);
-  // Не трогаем
-  return data;
-}
-
-// Место для третьей задачи
-function thirdTask() {
-  // Значения массива менять нельзя, тут он проверяется автоматически именно на эти значения
-  const data = [5, 10, "Shopping", 20, "Homework"];
-  const result = [];
-
-  // Пишем решение вот тут
-  for (let i = 1; i <= data.length; i++) {
-    result[i - 1] = data[data.length - i];
-  }
-
-  console.log(result);
-  // Не трогаем
-  return result;
-}
+console.log(result);
