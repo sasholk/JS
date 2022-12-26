19c93b32ce71f1e38757db1672e2f53b9242cc30
Анастасия Чернышова <anastasiacernysova@MacBook-Air-Anastasia.local>
2022-12-26 12:37:36 +0200

    rework elements

--- test/js/script.js
+++ test/js/script.js
@@ -1,2 +1,15 @@
 "use strict";
 
+let result = '';
+const length = 7;
+
+for (let i = 1; i < length; i++) {
+
+  for (let j = 0; j < i; j++) {
+    result += '*';
+  }
+
+  result += '\n';
+}
+
+console.log(result);
