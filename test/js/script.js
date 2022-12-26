ddced24f0f7b7ebca5e4f2b9d61898a3751526d6
sasholk <iccc97@gmail.com>
2022-12-26 15:45:13 +0200

    second practice  with Cycles in test folder

--- test/js/script.js
+++ test/js/script.js
@@ -1,58 +1,49 @@
 "use strict";
 
 function firstTask() {
-  // Пишем решение вот тут
-  let num = 5;
-  while (num < 11) {
-    console.log(num);
-    num++;
-  }
-}
-
-function secondTask() {
-  // Пишем решение вот тут
-  // let num = 20;
-  for (let i = 20; i > 9; i--) {
-    if (i === 12) {
-      break;
-    }
-    console.log(i);
-  }
-}
-
-function thirdTask() {
-  // Пишем решение вот тут
-  for (let i = 2; i < 11; i++) {
-    if (i % 2) {
-      continue;
-    }
-    console.log(i);
-  }
-}
-
-function fourthTask() {
-  // Пишем решение вот тут
-  let i = 2;
-  while (i <= 16) {
-    if (i % 2 === 0) {
-      i++;
-      continue;
-    } else {
-      console.log(i);
-    }
-    i++;
-  }
-}
-
-function fifthTask() {
-  const arrayOfNumbers = [];
+  // Значения массива менять нельзя, тут он проверяется автоматически именно на эти значения
+  const arr = [3, 5, 8, 16, 20, 23, 50];
+  const result = [];
 
   // Пишем решение вот тут
-  for (let i = 5; i < 11; i++) {
-    arrayOfNumbers[i - 5] = i;
+  for (let i = 0; i < arr.length; i++) {
+    result[i] = arr[i];
   }
-
-  console.log(arrayOfNumbers);
+  console.log(result);
   // Не трогаем
-  return arrayOfNumbers;
+  return result;
+}
+
+// Место для второй задачи
+function secondTask() {
+  // Значения массива менять нельзя, тут он проверяется автоматически именно на эти значения
+  const data = [5, 10, "Shopping", 20, "Homework"];
+
+  // Пишем решение вот тут
+  for (let i = 0; i < data.length; i++) {
+    if (typeof data[i] === "number") {
+      data[i] = data[i] * 2;
+    } else if (typeof data[i] === "string") {
+      data[i] = `${data[i]} - done`;
+    }
+  }
+  console.log(data);
+  // Не трогаем
+  return data;
+}
+
+// Место для третьей задачи
+function thirdTask() {
+  // Значения массива менять нельзя, тут он проверяется автоматически именно на эти значения
+  const data = [5, 10, "Shopping", 20, "Homework"];
+  const result = [];
+
+  // Пишем решение вот тут
+  for (let i = 1; i <= data.length; i++) {
+    result[i - 1] = data[data.length - i];
+  }
+
+  console.log(result);
+  // Не трогаем
+  return result;
 }
