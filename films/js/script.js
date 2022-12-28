43f8c7ce4e2a477f4530c38267ab57b1ac7d88ea
sasholk <iccc97@gmail.com>
2022-12-28 14:26:15 +0200

    added functions showMyDB, writeYourGenres

--- films/js/script.js
+++ films/js/script.js
@@ -50,4 +50,20 @@ function detectPersonalLevel() {
   }
 }
 
-console.log(personalMovieDB);
+detectPersonalLevel();
+
+function showMyDB(hidden) {
+  if (!hidden) {
+    console.log(personalMovieDB);
+  }
+}
+
+showMyDB(personalMovieDB.privat);
+
+function writeYourGenres() {
+  for (let i = 1; i <= 3; i++) {
+    personalMovieDB.genres[i - 1] = prompt(`Ваш любимый жанр под номером ${i}`);
+  }
+}
+
+writeYourGenres();
