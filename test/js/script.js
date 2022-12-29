b9fffc72a5c6f0726ce9e19b4ca166fc2a279777
sasholk <iccc97@gmail.com>
2022-12-29 13:17:43 +0200

    practice with func

--- test/js/script.js
+++ test/js/script.js
@@ -1,47 +1,51 @@
 "use strict";
 
-// 1) Создайте функцию, которая будет вычислять объем и площадь полной поверхности куба (тоже базовая математика, иногда используется в создании анимаций). Эта функция принимает в себя целое число со значением длины ребра куба. Ответ выведите в формате строки, который изображен в примерах.
-// Если в функцию попал неправильный аргумент или вычислить значения невозможно - вернуть строку "При вычислении произошла ошибка"
-// НЕ ИСПОЛЬЗУЙТЕ ОПЕРАТОР СТЕПЕНИ ** - в онлайн среде браузера он не работает и тесты будут ломаться. Это из-за того, что этот оператор из более нового стандарта, чем тут доступен.
-
-// 2) Постепенно переходим к более реалистичным задачам :) Вы обнаружите, что там используется все тоже самое.
-// Напишите функцию, которая будет определять номер купе по переданному ей номеру места.
-// Функция принимает только целое число от 1 до 36.
-// Если переданный аргумент не число, отрицательное или дробное - возвращается сообщение:
-// "Ошибка. Проверьте правильность введенного номера места"
-// Если число 0 или больше 36, то сообщение: "Таких мест в вагоне не существует"
 // Место для первой задачи
-function calculateVolumeAndArea(length) {
-  if (typeof length != "number" || length < 0 || !Number.isInteger(length)) {
-    return "При вычислении произошла ошибка";
+function getTimeFromMinutes(minutesTotal) {
+  if (
+    typeof minutesTotal !== "number" ||
+    minutesTotal < 0 ||
+    !Number.isInteger(minutesTotal)
+  ) {
+    return "Ошибка, проверьте данные";
   }
 
-  let volume = 0,
-    area = 0;
+  const hours = Math.floor(minutesTotal / 60);
+  const minutes = minutesTotal % 60;
 
-  volume = length * length * length;
-  area = 6 * (length * length);
+  let hoursStr = "";
 
-  return `Объем куба: ${volume}, площадь всей поверхности: ${area}`;
+  switch (hours) {
+    case 0:
+      hoursStr = "часов";
+      break;
+    case 1:
+      hoursStr = "час";
+      break;
+    case 2:
+    case 3:
+    case 4:
+      hoursStr = "часа";
+      break;
+    default:
+      hoursStr = "часов";
+  }
+
+  return `Это ${hours} ${hoursStr} и ${minutes} минут`;
 }
 
-calculateVolumeAndArea(5);
+getTimeFromMinutes(180);
 
 // Место для второй задачи
-function getCoupeNumber() {
+function findMaxNumber(a, b, c, d) {
   if (
-    typeof seatNumber !== "number" ||
-    seatNumber < 0 ||
-    !Number.isInteger(seatNumber)
+    typeof a !== "number" ||
+    typeof b !== "number" ||
+    typeof c !== "number" ||
+    typeof d !== "number"
   ) {
-    return "Ошибка. Проверьте правильность введенного номера места";
+    return 0;
+  } else {
+    return Math.max(a, b, c, d);
   }
-
-  if (seatNumber === 0 || seatNumber > 36) {
-    return "Таких мест в вагоне не существует";
-  }
-
-  return Math.ceil(seatNumber / 4);
 }
-
-getCoupeNumber(33);
